6818a3dd843de21c02232881d29dc47da9c18901
Dobby19 <southvi1@naver.com>
2025-06-29 21:04:13 +0900

    [FEAT] member 테이블 세팅

--- src/main/java/SpringAI/demo/login/Member.java
+++ src/main/java/SpringAI/demo/login/Member.java
@@ -9,5 +9,19 @@ public class Member {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  //이름
+  @Column(nullable = false, length = 30)
+  private String username;
 
+  //비밀번호
+  @Column(nullable = false, length = 20)
+  private String password;
+
+  //닉네임
+  @Column(nullable = false, length = 30)
+  private String name;
+
+  //이메일
+  @Column(nullable = false,length = 30)
+  private String email;
 }
