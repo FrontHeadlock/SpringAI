4a199e77bf391ef01a3fb443caab6f7614512fdd
Dobby19 <southvi1@naver.com>
2025-07-01 23:42:37 +0900

    [FEAT] chatBot 연동

--- src/main/java/SpringAI/demo/controller/ChatController.java
+++ src/main/java/SpringAI/demo/controller/ChatController.java
@@ -2,20 +2,26 @@ package SpringAI.demo.controller;
 
 import SpringAI.demo.service.OpenAIService;
 import java.util.Map;
-import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-@RestController
+@Controller
 public class ChatController {
 
   private final OpenAIService openAIService;
 
   public ChatController(OpenAIService openAIService) {
     this.openAIService = openAIService;
+  }
+
+  @GetMapping("/")
+  public String chatPage(){
+    return "chat";
   }
 
   @ResponseBody
